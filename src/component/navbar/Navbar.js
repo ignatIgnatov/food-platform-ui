c84df17e73ca445cb2c122e923126816bd502a44
ignatIgnatov <ignatov_rs@abv.bg>
2024-08-13 12:37:14 +0300

    implement navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,11 +1,43 @@
+import { Avatar, Badge, IconButton } from '@mui/material'
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import { pink } from '@mui/material/colors';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
+
+            <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
+                <li className='logo font-semibold text-gray-300 text-2xl list-none'>
+                    Food Platform
+                </li>
+            </div>
+
+            <div className='flex items-center space-x-2 lg:space-x-10'>
+                <div className=''>
+                    <IconButton>
+                        <SearchIcon sx={{ fontSize: "1.5rem" }} />
+                    </IconButton>
+                </div>
+                <div className=''>
+                    <IconButton>
+                        <Avatar sx={{ bgcolor: "white", color: pink.A400 }}>I</Avatar>
+                    </IconButton>
+                </div>
+                <div className=''>
+                    <IconButton>
+                        <Badge badgeContent={1}>
+                            <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
+                        </Badge>
+                    </IconButton>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Navbar
