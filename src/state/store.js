232dd155a737da1a73b665f70dcd7137c81f0549
Ignat <ignatov_rs@abv.bg>
2024-08-29 13:51:52 +0300

    implement menu item reducer

--- src/state/store.js
+++ src/state/store.js
@@ -2,11 +2,13 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "./authentication/Reducer";
 import { thunk } from "redux-thunk";
 import restaurantReducer from "./restaurant/Reducer";
+import menuItemReducer from "./menu/Reducer";
 
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    restaurant: restaurantReducer
+    restaurant: restaurantReducer,
+    menu: menuItemReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
